743c42f2c4d863382b6f35607baaed628ad900ca
Gennadiy Ganchev <gganchev@avistar.com>
2021-02-19 16:31:31 +0200

    Create symlink, asterisk can't find dir /etc/radiusclient-ng

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -5,6 +5,9 @@ trap "/stop.sh ; exit" EXIT
 
 chown -R kamailio:kamailio /etc/kamailio/
 
+# Create symlink, asterisk can't find /etc/radiusclient-ng
+ln -sfn /etc/radcli/ /etc/radiusclient-ng
+
 # Monit will start all apps
 /etc/init.d/monit restart
 monit start all
